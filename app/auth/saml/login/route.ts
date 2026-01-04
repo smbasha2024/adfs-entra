61223dee652544b78dd9460e62242d356ffd5ef2
Basha <smbasha2024@gmail.com>
2026-01-04 23:27:36 +0530

    SAML Login is changed

--- app/auth/saml/login/route.ts
+++ app/auth/saml/login/route.ts
@@ -8,19 +8,17 @@ function buildSamlRequest() {
   const id = "_" + uuidv4();
   const issueInstant = new Date().toISOString();
 
-  return `<samlp:AuthnRequest xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" ID="${id}" Version="2.0" IssueInstant="${issueInstant}" Destination="https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/saml2" AssertionConsumerServiceURL="https://adfsentra.onrender.com/auth/saml/callback"> <saml:Issuer>urn:adfsentra-saml</saml:Issuer> </samlp:AuthnRequest>`;
+  return `<samlp:AuthnRequest xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" ID="${id}" Version="2.0" IssueInstant="${issueInstant}" Destination="https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/saml2" AssertionConsumerServiceURL="https://adfsentra.onrender.com/auth/saml/callback" ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"> <saml:Issuer>urn:adfsentra-saml</saml:Issuer> <samlp:NameIDPolicy Format="urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress" AllowCreate="true"/> </samlp:AuthnRequest>`;
 }
 
 export async function GET() {
 
   const tenantId = process.env.AZURE_AD_TENANT_ID;
-  const appId = process.env.AUTH_SAML_APP_ID;
+  const appId = process.env.AUTH_SAML_APP_ID!;
 
   const xml = buildSamlRequest();
 
-  const deflated = zlib.deflateRawSync(xml);
-  const base64 = deflated.toString("base64");
-  const encoded = encodeURIComponent(base64);
+  const encoded = Buffer.from(xml, "utf-8").toString("base64");
 
   if (!tenantId) {
     return NextResponse.json(
@@ -29,11 +27,27 @@ export async function GET() {
     );
   }
 
-  const samlRequestUrl =
-    //`https://login.microsoftonline.com/${tenantId}/saml2?appid=${appId}`;
-    //`https://myapps.microsoft.com/signin/${appId}?tenantId=${tenantId}`;
+  /*
+  const redirectUrl =
     `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/saml2` +
-    `?SAMLRequest=${encoded}`;
+    `?SAMLRequest=${encodeURIComponent(encoded)}` +
+    `&RelayState=${encodeURIComponent("nextjs")}`;
 
-  return NextResponse.redirect(samlRequestUrl);
+  return NextResponse.redirect(redirectUrl, 302);
+  */
+
+  const html = `
+<!DOCTYPE html>
+<html>
+  <body onload="document.forms[0].submit()">
+    <form method="POST" action="https://login.microsoftonline.com/${tenantId}/saml2">
+      <input type="hidden" name="SAMLRequest" value="${encoded}" />
+      <input type="hidden" name="RelayState" value="nextjs" />
+    </form>
+  </body>
+</html>`;
+
+  return new NextResponse(html, {
+    headers: { "Content-Type": "text/html" },
+  });
 }
