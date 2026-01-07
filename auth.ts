9d6b13134158d10957810005ec75b9373b1f5d8f
Basha <smbasha2024@gmail.com>
2026-01-07 23:38:13 +0530

    client sec change

--- auth.ts
+++ auth.ts
@@ -7,6 +7,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     MicrosoftEntraID({
       clientId: process.env.AZURE_AD_CLIENT_ID!,
+      clientSecret: process.env.AZURE_CLIENT_SECRET!,
       issuer: `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/v2.0`,
       authorization: {
         params: {
